d429fa14c51f1fd33364e95a647772fb6d302f17
dylanbatar-co <dbatista@condorlabs.io>
2022-07-11 14:03:25 -0500

    :white_check_mark: Update incidents integration tests

--- src/main/routes/incident.route.spec.ts
+++ src/main/routes/incident.route.spec.ts
@@ -3,6 +3,7 @@ import server from '../config/app';
 import request from 'supertest';
 
 import { InMemoryRepository } from '../../external/repositories/inMemoryRepository/in-memory-repository';
+import { ServiceData } from '../../entities/service/service-data';
 
 describe('GET /history/:date', () => {
   let getIncidentsByMonthStub: SinonStub;
@@ -18,40 +19,86 @@ describe('GET /history/:date', () => {
   const BASE_URL = '/api/incident/history';
 
   it('Should return empty array if doesnt exist issues', async () => {
+    getIncidentsByMonthStub.returns([]);
     const response = await request(server).get(`${BASE_URL}/2022-01-01`);
     expect(response.body).toHaveLength(0);
   });
 
   it('Should return an array with last issues of last 3 months', async () => {
-    const fakeResponse = [
+    const fakeResponse: ServiceData[] = [
       {
-        'Jul 2022': {
-          id: '1',
-          name: 'Fake service 1',
-          fixed: true,
-          description: '',
-          date: '2022-05-01'
-        }
+        uuid: '1',
+        name: 'Fake service 1',
+        owner: 'Fake owener 1',
+        url: 'Fake url',
+        status: 'pass',
+        incidents: [
+          {
+            id: '1',
+            name: 'Fake incident service 1',
+            fixed: true,
+            description: '',
+            date: new Date('2022/06/01')
+          }
+        ]
       },
       {
-        'Jul 2022': {
-          id: '2',
-          name: 'Fake service 2',
-          fixed: true,
-          description: '',
-          date: '2022-06-01'
-        }
+        uuid: '2',
+        name: 'Fake service 2',
+        owner: 'Fake owener 2',
+        url: 'Fake url 2',
+        status: 'pass',
+        incidents: [
+          {
+            id: '2',
+            name: 'Fake incident service 2',
+            fixed: true,
+            description: '',
+            date: new Date('2022/05/12')
+          }
+        ]
       },
       {
-        'Jun 2022': {
-          id: '3',
-          name: 'Fake service 3',
-          fixed: true,
-          description: '',
-          date: '2022-05-01'
-        }
+        uuid: '3',
+        name: 'Fake service 3',
+        owner: 'Fake owener 3',
+        url: 'Fake url',
+        status: 'pass',
+        incidents: [
+          {
+            id: '3',
+            name: 'Fake incident service 3',
+            fixed: true,
+            description: '',
+            date: new Date('2022/09/01')
+          },
+          {
+            id: '4',
+            name: 'Fake incident service 4',
+            fixed: true,
+            description: '',
+            date: new Date('2022/06/01')
+          }
+        ]
+      },
+      {
+        uuid: '5',
+        name: 'Fake service 5',
+        owner: 'Fake owener 5',
+        url: 'Fake url 4',
+        status: 'pass',
+        incidents: [
+          {
+            id: '5',
+            name: 'Fake incident service 5',
+            fixed: true,
+            description: '',
+            date: new Date('2022/05/01')
+          }
+        ]
       }
     ];
+
     getIncidentsByMonthStub.returns(fakeResponse);
     const response = await request(server).get(`${BASE_URL}/2022-07-01`);
     expect(response.body).toHaveLength(3);
